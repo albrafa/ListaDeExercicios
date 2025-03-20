3f492a6121e5647484468b5f07b1b60158d4d793
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-20 18:08:54 -0300

    Adição do Exercício 05 (beta)

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio05
+{
+    internal class Program
+    {
+
+        //Crie um programa para calcular o salário total de um vendedor. Deverá ser informado o salário base e o total de
+        //vendas.A comissão é calculada com um percentual(informado pelo usuário) sobre o total de vendas.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o salário base do funcionário: ");
+            double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a porcentagem de comissão sobre a venda: ");
+            double comissaoPorVenda = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o total de vendas realizado pelo funcionário: ");
+            double totalVendas = 0;
+
+            double comissaoVendas = totalVendas + comissaoPorVenda;
+
+            double salarioTotal = salarioBase + comissaoVendas;
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
