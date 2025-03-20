ed2e2f39a167a299d7ddb25d4dbb3ad889766a60
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-20 16:05:01 -0300

    Finalizado exercício 02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+        //Crie um programa para calcular o volume de um Cilindro
+
+        ////Identifique o raio da base do cilindro e a altura
+        //Substitua os valores na fórmula V = π r² h
+        //Calcule o volume
+
+        // Volume = pi * raio * raio * altura
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o raio do cilindro: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a altura do cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double PI = Math.PI;
+
+            double volume = PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindro é {volume:f2} m³.");
+
+            Console.ReadLine();
+        }
+    }
+}
