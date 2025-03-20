02d8609ab78e8a9a5059d30ebc0f0768a6ded2c5
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-20 14:53:56 -0300

    Adicionado Exercício 01.

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio01
+{
+    internal class Program
+    {
+
+        // crie um programa para calculcar uma caixa retangular
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o comprimento da caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a larguta da caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            decimal resultado = comprimento * largura * altura;
+
+            Console.WriteLine($"O volume total da caixa é de {resultado} cm³.");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
