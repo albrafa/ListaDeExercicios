3f298b04bb5c56b0ea724dc2fa47d45df733b68b
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-21 10:52:52 -0300

    Finalização do exercício 05

--- Exercicio05/Program.cs
+++ Exercicio05/Program.cs
@@ -7,18 +7,24 @@
         //vendas.A comissão é calculada com um percentual(informado pelo usuário) sobre o total de vendas.
         static void Main(string[] args)
         {
+            Console.WriteLine("*----------------------------------*--------------------------------*");
+            Console.WriteLine("                         Calculadora de comissão");
+            Console.WriteLine("*----------------------------------*--------------------------------*");
+            Console.WriteLine();
+
             Console.WriteLine("Digite o salário base do funcionário: ");
             double salarioBase = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Digite a porcentagem de comissão sobre a venda: ");
-            double comissaoPorVenda = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o total de vendas realizado pelo funcionário (Em R$): ");
+            double totalVendas = Convert.ToDouble(Console.ReadLine()); ;
 
-            Console.WriteLine("Digite o total de vendas realizado pelo funcionário: ");
-            double totalVendas = 0;
+            Console.WriteLine("Digite a porcentagem de comissão sobre as vendas (apenas números): ");
+            double comissaoInformada = Convert.ToDouble(Console.ReadLine());
 
-            double comissaoVendas = totalVendas + comissaoPorVenda;
+            double comissaoVendas = (comissaoInformada * totalVendas) / 100;
 
             double salarioTotal = salarioBase + comissaoVendas;
+            Console.WriteLine($"O salário total do funcionário é de {salarioTotal}.");
             
             
             Console.ReadLine();
