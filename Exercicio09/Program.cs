3dd049fa2e757b3a62c8d3a94f014ee2dd503c4f
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-24 13:37:46 -0300

    Finalizada a atividade 09.

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio09
+{
+    internal class Program
+    {
+
+        //programa para calcular a area de um retangulo
+        // para calcular: Area = base x altura
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("            Calculadora de dimensões de terrenos retangulares");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+
+            Console.WriteLine("Digite o tamanho da base do terreno (apenas números): ");
+            double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o tamanho da altura do terreno (apenas números): ");
+            double alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            double areaRetangulo = alturaRetangulo * baseRetangulo;
+            Console.WriteLine($"A área do terreo é {areaRetangulo}m².");
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
