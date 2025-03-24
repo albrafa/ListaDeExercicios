5a9d11e4e92b3a44d5cbf736cbd23930ebfad53a
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-24 17:19:53 -0300

    Finalização da atividade 14.

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        //Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
+        {
+            Console.WriteLine("Algoritmo de classificação de valores por ordem numérica");
+
+            int[] numeros = [5, 2, 3];
+
+            Array.Sort(numeros);
+            Array.Reverse(numeros);
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
