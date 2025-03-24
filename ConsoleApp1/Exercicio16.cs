c2862b22a104c75971e561cce2f44316e1e8d320
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-24 17:51:54 -0300

    Finalização do exercício 16.

---
+++ ConsoleApp1/Exercicio16.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio16
+{
+    internal class Program
+    {
+
+        //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("                            PAR OU ÍMPAR");
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite um número.");
+            int numeroInformado = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroInformado % 2 == 0)
+            {
+                Console.WriteLine($"O número {numeroInformado} é um número par!");
+            }
+
+            else
+            {
+                Console.WriteLine($"O número {numeroInformado} é um número ímpar!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
