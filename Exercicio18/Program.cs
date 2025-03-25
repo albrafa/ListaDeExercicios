e87e87bbfa63b960ab54583434f9ce00d3f50946
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-03-25 13:22:33 -0300

    Finalização do exercício 18.

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio18
+{
+    internal class Program
+    {
+        //Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GERADOR DE NÚMEROS ÍMPARES");
+
+            Console.WriteLine();
+            Console.WriteLine("Aperte ENTER para iniciar o gerador... ");
+            Console.ReadLine();
+
+            
+            for (int i = 101; i <= 200; i +=2)
+            {
+                Console.WriteLine(i);
+            }
+                        
+
+            Console.ReadLine();
+        }
+    }
+}
